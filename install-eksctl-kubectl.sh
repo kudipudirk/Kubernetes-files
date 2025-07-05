0afef08908eeb40effe868f22240bd6c9fe0798b
kudipudirk <kudipudirk@gmail.com>
2025-07-05 15:37:40 +0530

    k8

--- install-eksctl-kubectl.sh
+++ install-eksctl-kubectl.sh
@@ -1,9 +1,8 @@
+#!/bin/bash
 #INSTALL EKSCTL
 sudo curl --silent --location "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
-sudo eksctl version
 #INSTALL KUBECTL
 sudo curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.0/2025-05-01/bin/linux/amd64/kubectl
 sudo chmod +x kubectl
 sudo mv kubectl /usr/local/bin
-sudo kubectl version --client
